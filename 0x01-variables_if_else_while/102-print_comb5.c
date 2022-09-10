caa3c68f87a2d8b1ba65e898457b0644d573805b
IBKBrako <brightkonadubrako@gmail.com>
2022-09-09 19:36:46 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
-				putchar(' ');	
+				putchar(' ');
 				putchar((p / 10) + '0');
 				putchar((p % 10) + '0');
 				if (i != 98)
